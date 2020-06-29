4de7b603466e66c1d40d51557f983c25ecf83dab
Kevin Lacker <lacker@gmail.com>
2020-06-29 11:30:36 -0700

    resized a target photo

--- resizer.py
+++ resizer.py
@@ -3,7 +3,8 @@ import PIL.Image
 def main():
     infile = "/home/lacker/Desktop/AJ.jpg"
     image = PIL.Image.open(infile)
-    print(image)
+    resized = image.resize((512, 512), PIL.Image.ANTIALIAS)
+    resized.save("results/aj.png")
 
 if __name__ == "__main__":
     main()
